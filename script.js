7f3f32639268b29057c89b50b962b87da87a3375
thcheetah777 <thcheetah777@gmail.com>
2022-01-07 15:11:44 -0800

    Add config

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function create() {
 function update() {
 
 }
+
+// Phaser config
+const config = {
+  type: Phaser.AUTO,
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.RESIZE
+  },
+  render: {
+    pixelArt: true
+  },
+  backgroundColor: 0xffffff,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 1500
+      },
+      enableBody: true,
+      debug: true
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
