90a968bf0e91ea947c7b7e91caa432c349dc4a50
thcheetah777 <thcheetah777@gmail.com>
2022-01-08 11:33:28 -0800

    Add debugger animation

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var game = {
     x: 0,
     y: 0
   },
-  mouseMoving: false
+  mouseMoving: false,
+  debuggerSearching: false
 };
 
 // Load all the sprites and background
@@ -32,6 +33,8 @@ function create() {
   game.player = this.physics.add.sprite(phaser.input.mousePointer.x, phaser.input.mousePointer.y, "one").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
   game.debugger = this.physics.add.sprite(0, 0, "debugger").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
   game.debugger.visible = false;
+  game.debugger.tweenX = 0;
+  game.debugger.tweenY = 0;
 
   // Check if mouse moving
   setInterval(function() {
@@ -48,11 +51,70 @@ function create() {
     const random = Math.floor(Math.random() * 2);
     let array = [{x: Math.floor(Math.random() * 2) ? 0 : config.width, y: Math.random() * config.height}, {x: Math.random() * config.width, y: Math.floor(Math.random() * 2) ? 0 : config.height}]
     let choice = array[random];
+    if (choice == array[0]) {
+      if (choice.x === 0) {
+        game.debugger.direction = "right";
+      } else {
+        game.debugger.direction = "left";
+      }
+    } else {
+      if (choice.y === 0) {
+        game.debugger.direction = "down";
+      } else {
+        game.debugger.direction = "up";
+      }
+    }
     game.debugger.x = choice.x;
     game.debugger.y = choice.y;
+    if (game.debugger.direction === "right") {
+      game.debugger.tweenX = game.debugger.x + 50;
+      game.debugger.tweenY = game.debugger.y;
+    } else if (game.debugger.direction === "left") {
+      game.debugger.tweenX = game.debugger.x - 50;
+      game.debugger.tweenY = game.debugger.y;
+    } else if (game.debugger.direction === "up") {
+      game.debugger.tweenX = game.debugger.x;
+      game.debugger.tweenY = game.debugger.y - 50;
+    } else {
+      game.debugger.tweenX = game.debugger.x;
+      game.debugger.tweenY = game.debugger.y + 50;
+    }
+    game.debugger.peekIn = phaser.tweens.add({
+      targets: game.debugger,
+      x: game.debugger.tweenX,
+      y: game.debugger.tweenY,
+      duration: 800,
+      callbackScope: this,
+      onComplete: function() {
+        game.debuggerSearching = true;
+      }
+    });
     setTimeout(function() {
-      game.debugger.visible = false;
-    }, 3000);
+      if (game.debugger.direction === "right") {
+        game.debugger.tweenX = game.debugger.x - 50;
+        game.debugger.tweenY = game.debugger.y;
+      } else if (game.debugger.direction === "left") {
+        game.debugger.tweenX = game.debugger.x + 50;
+        game.debugger.tweenY = game.debugger.y;
+      } else if (game.debugger.direction === "up") {
+        game.debugger.tweenX = game.debugger.x;
+        game.debugger.tweenY = game.debugger.y + 50;
+      } else {
+        game.debugger.tweenX = game.debugger.x;
+        game.debugger.tweenY = game.debugger.y - 50;
+      }
+      game.debugger.peekOut = phaser.tweens.add({
+        targets: game.debugger,
+        x: game.debugger.tweenX,
+        y: game.debugger.tweenY,
+        duration: 800,
+        callbackScope: this
+      });
+      setTimeout(function () {
+        game.debugger.visible = false;
+        game.debuggerSearching = false;
+      }, 800);
+    }, 2000);
   }, (Math.random() * 8000) + 3000);
 }
 
@@ -72,12 +134,12 @@ function update() {
 // Phaser config
 const config = {
   type: Phaser.AUTO,
-  width: 1000,
-  height: 800,
+  width: 1250,
+  height: 590,
   render: {
     pixelArt: true
   },
-  backgroundColor: 0x000000,
+  backgroundColor: 0xffffff,
   physics: {
     default: "arcade",
     arcade: {
